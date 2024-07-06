4ddffc9ee0a4586681fe5cded9a63d9385fd617d
Priyan Malakar <priyanmalakar123@gmail.com>
2024-07-06 12:07:30 +0530

    binary search implementation

---
+++ Searching Algorithm/Binary Search/binary_search_algorithm.c
@@ -0,0 +1,41 @@
+//implementation of binary search algorihtm using dynamic memory allocation
+#include<stdio.h>
+#include<stdlib.h>
+
+//iterative method
+int binary_search(int *arr,int n,int search_key){
+    int l=0,h=n-1;
+    while(l<=h){
+        int mid=l+(h-l)/2;
+        if(*(arr+mid)==search_key){
+            // printf("mid = %d\n",mid);
+            return mid;
+        }
+        if(*(arr+mid)<search_key){
+            l=mid+1;
+        }
+        if(*(arr+mid)>search_key){
+            h=mid-1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int *p,n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    p=(int*)malloc(n*sizeof(int));
+    printf("Enter the array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",(p+i));
+    }
+    int search_key;
+    printf("Enter the eleemnt to be searched: ");
+    scanf("%d",&search_key);
+    if(binary_search(p,n,search_key)!=-1){
+        printf("The element is found in %d index.\n",binary_search(p,n,search_key));
+    }else{
+        printf("Not found in the array.\n");
+    }
+    return 0;
+}
