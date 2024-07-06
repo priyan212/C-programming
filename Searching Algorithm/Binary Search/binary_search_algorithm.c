f760f8eae8b68b600dadbde6c0092a3b8a9770a0
Priyan Malakar <priyanmalakar123@gmail.com>
2024-07-06 12:16:36 +0530

    bsearch modified

--- Searching Algorithm/Binary Search/binary_search_algorithm.c
+++ Searching Algorithm/Binary Search/binary_search_algorithm.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 //iterative method
-int binary_search(int *arr,int n,int search_key){
+/* int binary_search(int *arr,int n,int search_key){
     int l=0,h=n-1;
     while(l<=h){
         int mid=l+(h-l)/2;
@@ -19,8 +19,18 @@ int binary_search(int *arr,int n,int search_key){
         }
     }
     return -1;
+} */
+
+int binary_search(int *p,int search_key,int l,int h){
+    if(l>=h){
+        return -1;
+    }
+    int mid = l+(h-l)/2;
+    if (*(p+mid)==search_key) return mid;
+    if (*(p+mid)<search_key) return binary_search(p,search_key,mid+1,h);
+    if (*(p+mid)>search_key) return binary_search(p,search_key,l,mid-1);
 }
-int main(){
+/* int main(){
     int *p,n;
     printf("Enter the size of the array: ");
     scanf("%d",&n);
@@ -34,6 +44,26 @@ int main(){
     scanf("%d",&search_key);
     if(binary_search(p,n,search_key)!=-1){
         printf("The element is found in %d index.\n",binary_search(p,n,search_key));
+    }else{
+        printf("Not found in the array.\n");
+    }
+    return 0;
+} */
+int main(){
+    int *p,n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    p=(int*)malloc(n*sizeof(int));
+    printf("Enter the array: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",(p+i));
+    }
+    int search_key;
+    printf("Enter the eleemnt to be searched: ");
+    scanf("%d",&search_key);
+    int index=binary_search(p,search_key,0,n-1);
+    if(index!=-1){
+        printf("The element is found in %d index.\n",index);
     }else{
         printf("Not found in the array.\n");
     }
